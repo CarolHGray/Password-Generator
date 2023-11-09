fb19be964b702624cd1dbb334b8fad1b95db087f
Carol Gray <56186028+CarolHGray@users.noreply.github.com>
2023-11-09 08:56:50 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,8 +19,8 @@ var pwdCriteria = {
   pwdNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
 
   //array to hold special characters
-  pwdCharacter: ["!", "\"", "#", "$", "%", "&", "\'", "*", "+",
-    "-", ".", "/", "\\", "<", ">", "=", "?", "@", "[", "]", "^", "`", "{", "}", "|", "~"]//24 symbols
+  pwdCharacter: ["!", "\", "#", "$", "%", "&", "\", "*", "+",
+    "-", ".", "/", "\\", "<", ">", "=", "?", "@", "[", "]", "{", "}", "|", "~"]//24 symbols
 }
 
 // Write password to the #password input on index.html
